2ab76eab66072c87bae585511b06643362e8de7c
AronPelgrim <aron.pelgrim@hva.nl>
2022-04-25 14:46:59 +0200

    Added node projecdt

---
+++ main.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const path = require('path')
+const io = require('socket.io')(http)
+const port = process.env.PORT || 4242
+
+app.use(express.static(path.resolve('public')))
+
+io.on('connection', (socket) => {
+    io.emit('connected')
+
+    socket.on('message', (message) => {
+        socket.broadcast.emit('message', message)
+    })
+
+    socket.on('username', (username) => {
+        socket.broadcast.emit('username', username)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('disconnected')
+    })
+
+    socket.on("typing", () => {
+        socket.broadcast.emit("typing")
+    })
+})
+
+http.listen(port, () => {
+    console.log('listening on port ', port)
+})
